91b3e366df15963c62e8682b2f6dad500dc4d9ee
ViridityMoon <viriditymoon@gmail.com>
2020-06-23 12:33:25 -0400

    Adding in validations

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,20 +1,22 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import axios from'axios';
 
+
     //you know
-// import * as Yup from 'yup';
+import * as Yup from 'yup';
+import signInFormSchema from './YupValidation/signInFormSchema';
 
 const initialFormValues = {
     username: '',
     password: '',
   };
 
-  const initialFormErrors = {
+const initialFormErrors = {
     username: '',
     password: '',
-
-  };
+}
 
 export default function SignUpForm() {
     
@@ -22,31 +24,84 @@ export default function SignUpForm() {
     const [formErrors, setFormErrors] = useState(initialFormErrors);
     const [disabled, setDisabled] = useState(true);
 
+    let history = useHistory();
+
     const onInputChange = evt => {
     
         const { name, value } = evt.target
+
+        Yup
+          .reach(signInFormSchema, name)
+    
+          .validate(value)
+    
+          .then(() => {
+            setFormErrors({
+              ...formErrors,
+              [name]: ""
+            })
+          })
+          .catch(err => {
+            setFormErrors({
+              ...formErrors,
+              [name]: err.errors[0]
+            })
+          })
+    
+        setFormValues({
+          ...formValues,
+          [name]: value
+        })
+    };
+
+
+    const signInUser = (user) => {
+      axios.post('https://secret-family-recipes-6.herokuapp.com/auth/login', user)
+          .then(res => {
+            console.log(res);
+            localStorage.setItem('token', res.data.token);
+            history.push('/');
+          })
+          .catch(err => {
+            console.log(err)
+          })
+          .finally(() => {
+            setFormValues(initialFormValues);
+          })
+    };
+
+    const onSubmit = evt => {
+      evt.preventDefault();
+    
+      const user = {
+        username: formValues.username.trim(),
+        password: formValues.password,
       };
+    
+      signInUser(user);
+    };
 
+    const toSignUp = evt => {
+      evt.preventDefault();
 
-      // This is my generic post request, not sure how I'll be using it yet, or if it's for you.
-    // const postUsers = newUser => {
-    //     axios.post('', newUser)
-    //       .then(response => {
-    //         setUsers([response.data, ...users]);
-    //         debugger
-    //       })
-    //       .catch(err => {
-    //         console.log(err)
-    //       })
-    //       .finally(() => {
-    //         setFormValues(initialFormValues);
-    //       })
-    //   };
-      
+      history.push('/signup');
+    };
+
+    // This is to refresh the formValues for validation later
+    useEffect(() => {
+
+      signInFormSchema.isValid(formValues).then(props => {
+        setDisabled(!props)
+      })
+    }, [formValues]);
 
 
   return (
     <div className="form-container">
+        <div className='errors'>
+            <div>{formErrors.username}</div>
+            <div>{formErrors.password}</div>
+        </div>
         <div className='form-input'>
             <h3>Sign In</h3>
             <label> Username
@@ -57,6 +112,8 @@ export default function SignUpForm() {
             </label>
             <button onClick={onSubmit}>Sign In</button>
         </div>
+        <p>New Here? Click to Register</p>
+        <button onClick={toSignUp}>Register Here</button>
     </div>
 );
 }
