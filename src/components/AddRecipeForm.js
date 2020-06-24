b9387a15766fcb1f849a2875b58e594cf0118b62
Barbara Moore <barbaraloismoore@gmail.com>
2020-06-24 02:29:01 -0400

    render recipe details on click. add/edit forms made. some small state changes in app

--- src/components/AddRecipeForm.js
+++ src/components/AddRecipeForm.js
@@ -1,5 +1,112 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Forms.css';
+
+const initialFormValues = {
+  title: '',
+  source: '',
+  category: '',
+  recipe_img: '',
+  ingredients: '',
+  instructions: '',
+};
 
 export default function AddRecipeForm() {
-  return <h3>Add form will go here</h3>;
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const onInputChange = (event) => {
+    const { name } = event.target;
+    const { value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(formValues);
+    // axiosWithAuth()
+    //   .post(`/recipes`, formValues)
+    //   .then((res) => {
+    //     console.log('new recipe submitted');
+    //     history.push(`/recipes/${res.data (id returned??) }`);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+  };
+
+  return (
+    <form onSubmit={onSubmit} className="update">
+      <h2>Add a New Recipe</h2>
+
+      <label>
+        Title:&nbsp;
+        <input
+          type="text"
+          placeholder="..."
+          maxLength="50"
+          name="title"
+          value={formValues.title}
+          onChange={onInputChange}
+        />
+      </label>
+
+      <label>
+        Source:&nbsp;
+        <input
+          type="text"
+          name="source"
+          placeholder="..."
+          maxLength="50"
+          value={formValues.source}
+          onChange={onInputChange}
+        />
+      </label>
+
+      <label>
+        Category:&nbsp;
+        <input
+          type="text"
+          name="category"
+          placeholder="..."
+          maxLength="50"
+          value={formValues.category}
+          onChange={onInputChange}
+        />
+      </label>
+
+      <label>
+        Image URL:&nbsp;
+        <input
+          type="text"
+          name="recipe_img"
+          placeholder="..."
+          maxLength="100"
+          value={formValues.recipe_img}
+          onChange={onInputChange}
+        />
+      </label>
+
+      <label>
+        Ingredients:&nbsp;
+        <input
+          type="text"
+          name="ingredients"
+          placeholder="..."
+          value={formValues.ingredients}
+          onChange={onInputChange}
+        />
+      </label>
+
+      <label>
+        Instructions:&nbsp;
+        <input
+          type="text"
+          name="instructions"
+          placeholder="..."
+          value={formValues.instructions}
+          onChange={onInputChange}
+        />
+      </label>
+
+      <button>Submit</button>
+    </form>
+  );
 }
--- src/components/EditRecipeForm.js
+++ src/components/EditRecipeForm.js
@@ -1,5 +1,125 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { RecipesContext } from '../contexts/RecipesContext';
+import { useHistory, useParams } from 'react-router-dom';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+const initialFormValues = {
+  title: '',
+  source: '',
+  category: '',
+  recipe_img: '',
+  ingredients: '',
+  instructions: '',
+};
 
 export default function EditRecipeForm() {
-  return <h3>Edit form will go here</h3>;
+  const { currentRecipe } = useContext(RecipesContext);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const history = useHistory();
+  const { id } = useParams();
+
+  const onInputChange = (event) => {
+    const { name } = event.target;
+    const { value } = event.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  useEffect(
+    (currentRecipe) => {
+      setFormValues({ ...currentRecipe });
+    },
+    [id]
+  );
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(formValues);
+    // axiosWithAuth()
+    //   .put(`/recipes/${id}`, formValues)
+    //   .then((res) => {
+    //     console.log('edit submitted');
+    //     history.push(`/recipes/${id}`);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+  };
+
+  return (
+    <form onSubmit={onSubmit} className="update">
+      <h2>Update {currentRecipe.title}</h2>
+
+      <label>
+        Title:&nbsp;
+        <input
+          type="text"
+          placeholder="..."
+          maxLength="50"
+          name="title"
+          value={formValues.title}
+          onChange={onInputChange}
+        />
+      </label>
+
+      <label>
+        Source:&nbsp;
+        <input
+          type="text"
+          name="source"
+          placeholder="..."
+          maxLength="50"
+          value={formValues.source}
+          onChange={onInputChange}
+        />
+      </label>
+
+      <label>
+        Category:&nbsp;
+        <input
+          type="text"
+          name="category"
+          placeholder="..."
+          maxLength="50"
+          value={formValues.category}
+          onChange={onInputChange}
+        />
+      </label>
+
+      <label>
+        Image:&nbsp;
+        <input
+          type="text"
+          name="recipe_img"
+          placeholder="..."
+          maxLength="100"
+          value={formValues.recipe_img}
+          onChange={onInputChange}
+        />
+      </label>
+
+      <label>
+        Ingredients:&nbsp;
+        <input
+          type="text"
+          name="ingredients"
+          placeholder="..."
+          value={formValues.ingredients}
+          onChange={onInputChange}
+        />
+      </label>
+
+      <label>
+        Instructions:&nbsp;
+        <input
+          type="text"
+          name="instructions"
+          placeholder="..."
+          value={formValues.instructions}
+          onChange={onInputChange}
+        />
+      </label>
+
+      <button>Submit</button>
+    </form>
+  );
 }
